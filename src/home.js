520c59f82dd2b2941d9c54560c61bf344bd3bd6f
Tyler Weitler <tweitler@gmail.com>
2023-03-05 21:04:03 -0500

    Added some content to home page
    Adjusted positioning on menu page as well

--- src/home.js
+++ src/home.js
@@ -2,10 +2,22 @@ function createHome() {
     const home = document.createElement('div');
     home.classList.add('home');
 
-    const header = document.createElement('h2');
-    header.textContent = 'Home';
+    const paragraph = document.createElement('p');
+    paragraph.textContent = 'Best imaginary food never created';
 
-    home.appendChild(header);
+    const para2 = document.createElement('p');
+    para2.textContent = 'Established in 2023';
+
+    const para3 = document.createElement('p');
+    para3.textContent = 'Check contact us for more details!';
+
+    const img = document.createElement('img');
+    img.src = 'images/frenchfries.jpg'
+
+    home.appendChild(paragraph);
+    home.appendChild(para2);
+    home.appendChild(img);
+    home.appendChild(para3);
 
     return home;
 }
